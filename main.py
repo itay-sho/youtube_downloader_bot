b58a318e7d5565a934f03ed844fbf29f676d09c8
Itay Shoshani <itai.sho@gmail.com>
2018-01-28 02:53:53 +0200

    develop: updating to latest version, working in AWS lambda

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
-import enum
+import os
+import sys
+
+sys.path.append(os.path.join(os.environ['LAMBDA_TASK_ROOT'], 'env/lib/python3.6/site-packages'))
+sys.path.append(os.environ['LAMBDA_TASK_ROOT'])
+os.environ['PATH'] += '{old_path}:{new_path}'.format(old_path=os.environ['PATH'], new_path=os.path.join(os.environ['LAMBDA_TASK_ROOT'], 'binaries'))
+
+import json
+import logging
 
 import telegram_bot
 import youtube_downloader
 import decorators
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    try:
+        logger.info('Event: {}'.format(event))
+        logger.info('Context: {}'.format(context))
+
+        body = json.loads(event['body'])
+        chat = body['message']['chat']
+        text = body['message']['text']
+
+        logger.info('PATH: {}'.format(os.environ['PATH']))
+
+        logger.info('Body: {}'.format(body))
+        logger.info('Chat: {}'.format(chat))
+        logger.info('Chat ID: {}'.format(chat['id']))
+        logger.info('Message: {}'.format(text))
+
+        bot = telegram_bot.TelegramBot('tokens/telegram_token.txt')
+
+        if text.split()[0] == '/youtube_to_mp3':
+            telegram_bot.TelegramBot.youtube_to_mp3(bot.bot, chat, text)
+        else:
+            bot.bot.send_message(chat_id=chat['id'], text='Ack')
+    finally:
+        return {}
 
 
 def main():
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,5 +1,6 @@
 from telegram.ext import MessageHandler, CommandHandler, Filters
 from telegram.ext import Updater
+from telegram import Bot
 import logging
 import enum
 
@@ -17,13 +18,15 @@ class TelegramBot:
         with open(token_filename, 'rt') as token_file:
             self.token = token_file.read().splitlines()[0]
 
+        self.bot = Bot(token=self.token)
+
     def launch(self):
         updater = Updater(token=self.token)
         dispatcher = updater.dispatcher
         logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
         start_handler = CommandHandler('start', TelegramBot.start)
-        youtube_to_mp3_handler = CommandHandler('youtube_to_mp3', TelegramBot.youtube_to_mp3)
+        youtube_to_mp3_handler = CommandHandler('youtube_to_mp3', TelegramBot.youtube_to_mp3_handler)
         dispatcher.add_handler(start_handler)
         dispatcher.add_handler(youtube_to_mp3_handler)
 
@@ -39,25 +42,54 @@ class TelegramBot:
         return True
 
     @staticmethod
-    def youtube_to_mp3(bot, update):
+    def youtube_to_mp3_handler(bot, update):
         try:
             args_list = update.message.text.split()
+            chat_id = update.message.chat_id
 
             if len(args_list) != YoutubeToMp3Arguments.COUNT.value:
-                bot.send_message(chat_id=update.message.chat_id, text='Invalid arguments count')
+                bot.send_message(chat_id=chat_id, text='Invalid arguments count')
                 return
 
             youtube_link = args_list[YoutubeToMp3Arguments.YOUTUBE_LINK.value]
 
             if not TelegramBot.validate_youtube_link_argument(youtube_link):
-                bot.send_message(chat_id=update.message.chat_id, text='Invalid argument')
+                bot.send_message(chat_id=chat_id, text='Invalid argument')
                 return
 
-            bot.send_message(chat_id=update.message.chat_id, text='Starting to convert video')
+            bot.send_message(chat_id=chat_id, text='Starting to convert video')
             output_file = youtube_downloader.YoutubeDownloader.download_and_convert(args_list[YoutubeToMp3Arguments.YOUTUBE_LINK.value])
 
             with open(output_file, 'rb') as downloaded_audio:
-                bot.send_audio(chat_id=update.message.chat_id, audio=downloaded_audio, timeout=100)
+                bot.send_audio(chat_id=chat_id, audio=downloaded_audio, timeout=100)
 
         except Exception as e:
-            bot.send_message(chat_id=update.message.chat_id, text='Error: {}'.format(e))
+            bot.send_message(chat_id=chat_id, text='Error: {}'.format(e))
+
+    @staticmethod
+    def youtube_to_mp3(bot, chat_id, text):
+        try:
+            logging.error("Starting calculate")
+
+            args_list = text.split()
+
+            if len(args_list) != YoutubeToMp3Arguments.COUNT.value:
+                bot.send_message(chat_id=chat_id, text='Invalid arguments count')
+                return
+
+            youtube_link = args_list[YoutubeToMp3Arguments.YOUTUBE_LINK.value]
+
+            if not TelegramBot.validate_youtube_link_argument(youtube_link):
+                bot.send_message(chat_id=chat_id, text='Invalid argument')
+                return
+
+            bot.send_message(chat_id=chat_id, text='Starting to convert video')
+            output_file = youtube_downloader.YoutubeDownloader.download_and_convert(
+                args_list[YoutubeToMp3Arguments.YOUTUBE_LINK.value])
+
+            with open(output_file, 'rb') as downloaded_audio:
+                bot.send_audio(chat_id=chat_id, audio=downloaded_audio, timeout=100)
+
+        except Exception as e:
+            logging.error("Exception: {}", e)
+            bot.send_message(chat_id=chat_id, text='Error: {}'.format(e))
